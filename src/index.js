3fffeb2ca294e72d938819d9e5b0202f863b2d4c
koba <traorekoba7@gmail.com>
2026-03-11 18:19:49 +0100

    [ADD] commit de ESLint, Husky et du workflow

---
+++ src/index.js
@@ -0,0 +1,10 @@
+document.getElementById('app').innerHTML = `
+  <h1>Hello World!</h1>
+  <p>Welcome to your Vanilla JS app.</p>
+  <p>Start editing to see some magic happen :)</p>
+`;
+
+const a = 1;
+const b = 2
+const c = a + b;
+console.log(c);
